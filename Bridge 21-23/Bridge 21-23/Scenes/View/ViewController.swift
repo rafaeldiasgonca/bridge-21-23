da28b55a790e424d6c82e50cbabbc5412c406e83
rafaeldiasgonca <66381340+rafaeldiasgonca@users.noreply.github.com>
2021-09-21 13:16:49 -0400

    Add files via upload

---
+++ Bridge 21-23/Bridge 21-23/Scenes/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bridge 21-23
+//
+//  Created by Rafael Dias Gonçalves on 20/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bridge 21-23/Bridge 21-23/Scenes/ViewModel/VIewModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Bridge 21-23
+//
+//  Created by Rafael Dias Gonçalves on 21/09/21.
+//
+
+import Foundation
